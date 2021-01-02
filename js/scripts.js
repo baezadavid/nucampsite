b945a0aab5a6eb33ca2142ca824728355e76c658
David Baeza <david.isaac.baeza@gmail.com>
2021-01-02 11:39:33 -0600

    Week 4 assignment

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,11 @@ $(function() {
      });
 
  });
+
+     $('#reserveButton').click(function() {
+        $('#reserveModal').modal('show');
+     });
+
+     $('#loginButton').click(function() {
+        $('#loginModal').modal('show');
+     });
